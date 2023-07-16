9585540ab3ec9b0723886a46552ea11e5b8a7a57
Sina Meysami <78996423+mrprogrammer2938@users.noreply.github.com>
2023-07-16 14:01:50 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,13 @@ fi
 clear
 echo "Installing..."
 sleep 2
-chmod +x sound.py
+chmod +x main.py
 apt install python
 apt install python3
 apt install python3-pip
 pip3 install --upgrade pip3
+pip3 install PyQt5
+pip3 install pygame
 echo ""
 echo "-------- Finish ---------"
 echo ""
